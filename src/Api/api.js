aaa4487880f83e71f6014089b0f4b511c3cea521
joaocdfarias <joaocdfariass@gmail.com>
2021-03-29 13:45:58 -0300

    add: api for experiences

---
+++ src/Api/api.js
@@ -0,0 +1,11 @@
+export const ExperienceApi = [
+  {
+    id: 0,
+    company: "Freelancer",
+    jobName: "Polvo Consciente",
+    url: "http://polvoconsciente.com/",
+    description:
+      "Site criado com JavaScript puro, utilizando a API do WordPress para criação de novos posts. Os posts são mostrados aleatoriamente ao clique de um botão, em uma espécie de roleta da sorte com uma nova frase e imagem",
+    resposability: "Desevolvedor Front-end",
+  },
+];
