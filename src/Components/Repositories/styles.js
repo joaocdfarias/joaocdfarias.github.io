b0505a31763ae5d44deb756d651c0bd7b42bda8d
joaocdfarias <joaocdfariass@gmail.com>
2021-03-29 13:47:54 -0300

    style: repositories component

---
+++ src/Components/Repositories/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const RepoWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin-top: 20px;
+  width: 600px;
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+export const Repo = styled.div`
+  background: linear-gradient(90deg, #7211aa 0%, #590d85 100%);
+  padding: 20px;
+  width: 290px;
+  text-align: center;
+  border-radius: 5px;
+  height: 110px;
+  transition: 0.3s;
+
+  :hover {
+    box-shadow: 5px 5px 15px -5px #590d85;
+    transition: 0.3s;
+  }
+`;
