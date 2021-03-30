fd005696a99f4884781dce69a62c774ba896346e
joaocdfarias <joaocdfariass@gmail.com>
2021-03-29 23:10:33 -0300

    add: title

---
+++ src/Components/About/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Title, AboutWrapper } from "./styles";
+
+function About() {
+  return (
+    <AboutWrapper>
+      <Title>Quem sou eu?</Title>
+    </AboutWrapper>
+  );
+}
+
+export default About;
