5d0086d0785d9d453b1e54f304db2dd5da18ad8c
joaocdfarias <joaocdfariass@gmail.com>
2021-03-30 23:11:49 -0300

    add: text about me

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -5,6 +5,13 @@ function About() {
   return (
     <AboutWrapper>
       <Title>Quem sou eu?</Title>
+      <p>
+        Meu nome é João Carlos, tenho 20 anos, sou desenvolvedor
+        front-end/freelancer de Salvador, BA. Trabalho com desenvolvimento a
+        pouco mais de 1 ano e utilizo tecnologias com JavaScript, React, Next,
+        Express, SQL e WordPress. Trabalho também como UI Designer, responsável
+        por criar layouts para sites e aplicativos mobile.
+      </p>
     </AboutWrapper>
   );
 }
